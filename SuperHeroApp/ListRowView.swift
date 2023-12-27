b6d06103728d276700ed6fcc87a0cf88ebc547d1
Mustafa Cakir <98mustafacan@gmail.com>
2023-12-27 20:27:29 +0300

    0.1

--- SuperHeroApp/ListRowView.swift
+++ SuperHeroApp/ListRowView.swift
@@ -11,19 +11,18 @@ struct ListRowView: View {
     var superhero: superHero
     var body: some View {
         HStack{
+            VStack{
+                Text(superhero.superHeroName).font(.system(size: 25))
+                Text(superhero.superHeroRealName).font(.system(size: 20))
+            }
+            Spacer()
+            
             Image(superhero.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.white,lineWidth: 5))
                 .frame(minWidth: 100, idealWidth: 100, maxWidth: 50, minHeight: 100, idealHeight: 100, maxHeight: 50, alignment: .center)
-                
-        
-            VStack{
-                Text(superhero.superHeroName).font(.title)
-                Text(superhero.superHeroRealName)
-            }
-            Spacer()
         }
     }
 }
